9b44667b80a293f2991b8152a19de821cfa59aa2
niktexnik <niktiek@yandex.ru>
2021-04-11 18:35:37 +0300

    go

--- todojs copy.js
+++ todojs copy.js
@@ -1,23 +1,57 @@
 window.onload = function () {
   let todos = [];
   let form = document.forms.my;
-  let input = document.getElementById("maininput");
   let total = document.getElementById("total");
-  let startId = 1;
+  let startId = 1000;
 
-  const handleComplete = (id) => {
-    const todoIndex = todos.findIndex((el) => el.id === id);
+  const setTodos = (newTodos) => {
+    todos = newTodos
+  }
+
+  const createTodo = (text) => {
+    const newTodo = {
+      id: startId++,
+      title: text,
+      done: false,
+      important: false
+    }
+
+    setTodos([...todos, newTodo]);
+  }
+
+  const completeTodo = (todoId) => {
+    const todoIndex = todos.findIndex((el) => el.id === todoId);
     const newTodo = todos[todoIndex];
     const done = newTodo.done;
     newTodo.done = !done;
-    todos = [...todos.slice(0, todoIndex), newTodo, ...todos.slice(todoIndex + 1)];
+    setTodos([...todos.slice(0, todoIndex), newTodo, ...todos.slice(todoIndex + 1)]);
+  };
+
+  const deleteTodo = (id) => {
+    const todoIndex = todos.findIndex((el) => el.id === id);
+    setTodos([...todos.slice(0, todoIndex), ...todos.slice(todoIndex + 1)]);
+  }
+
+  const handleComplete = (id) => {
+    completeTodo(id)
+    localStorage.setItem('todoing', JSON.stringify(todos));//localstorage
+    renderTodoList(todos);
+  }
+
+  const handleDelete = (todoId) => {
+    deleteTodo(todoId)
+    localStorage.setItem('todoing', JSON.stringify(todos));//localstorage
     renderTodoList(todos);
   }
 
   const renderTodoList = (todosCollection) => {
     let todoList = document.getElementById("list");
     todoList.innerHTML = '';
-
+    if (todosCollection.length === 0) {
+      let emptyResultsSpan = document.createElement("span")
+      emptyResultsSpan.textContent = 'Please add your first item...'
+      todoList.appendChild(emptyResultsSpan)
+    }
     todosCollection.map((todo) => {
       const title = todo.title;
       const todoId = todo.id;
@@ -29,11 +63,7 @@ window.onload = function () {
       let copmleteBtn = document.createElement("button");
 
       deleteBtn.className = "btn btn-outline-danger btn-sm float-right fa fa-trash-o";
-      deleteBtn.addEventListener("click", (event) => {
-        const todoIndex = todos.findIndex((el) => el.id === todoId);
-        todos = [...todos.slice(0, todoIndex), ...todos.slice(todoIndex + 1)]
-        renderTodoList(todos);
-      })
+      deleteBtn.addEventListener("click", (event) => handleDelete(todoId))
 
       copmleteBtn.className = `btn btn-outline-success btn-sm float-right fa ${completeBtnClass}`
       copmleteBtn.addEventListener("click", (event) => handleComplete(todoId))
@@ -49,25 +79,22 @@ window.onload = function () {
     total.innerHTML = todos.length;
   }
 
+  let todosFromLocalStorage = [];
+  try {todosFromLocalStorage = JSON.parse(localStorage.getItem('todoing'))} catch {}
+
+  if (Array.isArray(todosFromLocalStorage) && todosFromLocalStorage.length > 0) {
+    setTodos(todosFromLocalStorage);
+  }
+
   renderTodoList(todos);
 
   form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    let str = input.value;
+    const input = document.getElementById('maininput');
+    createTodo(input.value);
     input.value = '';
-
-    createTodo(str)
-  })
-
-  function createTodo(text) {
-    todos.push({
-      id: startId++,
-      title: text,
-      done: false,
-      important: false
-    })
-
+    localStorage.setItem('todoing', JSON.stringify(todos))
     renderTodoList(todos);
-  }
+  })
 }
